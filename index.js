efc108dc7a0880b8e97ddf2e3d9857a8fa59675f
jordipernas <jordikpernas00@gmail.com>
2023-09-05 21:12:44 -0300

    primerentregafinal

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 function creditoPersonal() {
-    const nombre = prompt("¿Cuál es tu nombre?: ");
+    const nombre = prompt("Bienvenido! ¿Cuál es tu nombre?: ");
     const apellido = prompt("¿Cuál es tu apellido?: ");
     const monto = prompt("¿Cuánto dinero solicitas?: ");
   
     if (monto <= 0) {
-      alert("El monto debe ser un número mayor a 0.");
-      return;
+        alert("El monto debe ser un número mayor a 0.");
+        return;
     }
-  
-    console.log("Bienvenido al simulador de crédito personal.");
-  
-    const plazos = [6, 9, 12, 18, 24];
-    for (let i = 0; i < plazos.length; i++) {
-      console.log(`Elige una opción de plazo: ${i + 1}. ${plazos[i]} cuotas`);
-    }
-  
-    const plazo = prompt("Ingresa la cantidad de cuotas que deseas:  6, 12, 18, 24 ");
-  
+
+    const plazo = prompt("Ingresa la cantidad de cuotas que deseas: 6, 12, 18 o 24");
+
     if (plazo <= 0) {
-      alert("La cantidad de cuotas debe ser un número mayor a 0.");
-      return;
+        alert("La cantidad de cuotas debe ser un número mayor a 0.");
+        return;
+    } else if (plazo > 24) {
+        alert("La cantidad de cuotas no debe ser mayor a 24.");
+        return;
+    } if ( (plazo <= 5 ) && ( plazo >= 11 ) && ( plazo <= 17 ) && ( plazo >=23 ) ) {
+        alert("La cantidad de cuotas tiene que ser 6, 12, 18 o 24.");
+        return;
     }
-  
+
     const cuota = monto / plazo;
-  
+
     alert(`Solicitud de crédito personal de: ${monto} pesos
   
   A nombre de: ${nombre} ${apellido}
@@ -32,5 +31,4 @@ function creditoPersonal() {
   
   Plazo elegido: ${plazo} cuotas`);
   }
-  
   creditoPersonal();
