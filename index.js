548b176bac2062c9ec2028c5f439d68be3bafba7
jordipernas <jordikpernas00@gmail.com>
2023-09-21 19:51:46 -0300

    3 preentregajordipernas

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ let respuesta = "si";
 
 while (respuesta == "si" || respuesta == "sí") {
 
-let nombre = prompt("Bienvenido! ¿Cuál es tu nombre?: ");
-let apellido = prompt("¿Cuál es tu apellido?: ");
+function persona (nombre, apellido, edad) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.setName = function (newName) {
+        this.nombre = newName;
+    }
+}
+const nombre = prompt("Bienvenido! ¿Cuál es tu nombre?: ");
+const apellido = prompt("¿Cuál es tu apellido?: ");
+const edad = prompt("ingrese su edad.")
+const persona1 = new persona (nombre, apellido, edad);
+
+console.log(persona1);
+
+for (key in persona1) {
+    console.log(persona1[key]);
+}
 let plata = prompt("¿Cuánto dinero solicitas?: ");
+
 const monto = ((plata <= 0) && (plata >1000000));
 
 if (plata <= 0){
@@ -17,21 +34,25 @@ if (plata <= 0){
 } 
 
 
-let cuota = parseInt(prompt("Ingresa la cantidad de cuotas que deseas: 6, 12, 18 o 24"));
-if (cuota < 0 || cuota > 24) {
-    alert("Hacemos créditos con hasta 24 cuotas");
-    break;
-  } else {
-        
-    alert("El número de cuotas seleccionado es: " + cuota);
+const cuota = [6, 12, 18, 24];
+const cantidad = parseInt(alert("Le comentamos que por el momento hacemos creditos en 6, 12 , 18 y 24 cuotas, a continuacion le mostramos como queda su credito en esos plazos"));
+
+function porCadaElemento(arr, accion) {
+    for (let i = 0; i < arr.length; i++) {
+        accion(arr[i])
+    }
 }
 
-function calcularPrestamo(a, b) {
-    return((a / b)*1.18);
-  }
-let resultado = calcularPrestamo (plata, cuota);
+const resultado =[];
+
+cuota.forEach((el) => resultado.push((plata*1.28)/el));
+
+
     alert(nombre + " " + apellido + " le comentamos que el resultado de su prestamo serian " + resultado + " al mes.");
 
   
     respuesta = prompt("¿Quieres calcular otra vez? (si/no)");
-}
+} 
+
+
+
